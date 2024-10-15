bc69288f9aa5512edc95b401b3f3316d984d5547
oleksandraankusheva <119884196+oleksandraankusheva@users.noreply.github.com>
2024-10-15 14:14:33 +0300

    TS-004: Added class Point and TaxiFinder (#9)

--- src/main/java/org/example/Point.java
+++ src/main/java/org/example/Point.java
@@ -1,9 +1,13 @@
 package org.example;
 
-public class Point{
+public class Point {
     double x;
     double y;
-    public Point(double x, double y){
+
+    public Point(double x, double y) {
+        if (x < 0 || y < 0) {
+            throw new IllegalArgumentException("Negative coordinates.");
+        }
         this.x = x;
         this.y = y;
     }
--- src/main/java/org/example/TaxiFinder.java
+++ src/main/java/org/example/TaxiFinder.java
@@ -7,19 +7,20 @@ import java.util.List;
 public class TaxiFinder {
     public static void main(String[] args) {
         List<Taxi> taxis = new ArrayList<>();
-        taxis.add(new Taxi(1, new Point(2, 3)));
+        taxis.add(new Taxi(1, new Point(1, 3)));
         taxis.add(new Taxi(2, new Point(5, 1)));
 
         Order order = new Order(new Point(3, 4));
 
         Taxi nearestTaxi = findNearestTaxi(taxis, order);
-        System.out.println("Найближче таксі: " + nearestTaxi.id);
+        System.out.println("Near Taxi: " + nearestTaxi.id);
         }
 
         public static double calculateDistance(Point p1, Point p2)
         {
             return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
         }
+
     public static Taxi findNearestTaxi(List<Taxi> taxis, Order order) {
         Taxi nearestTaxi = null;
         double minDistance = Double.MAX_VALUE;
